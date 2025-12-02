bcdc9c5cb8ce97bcfa3f7ddf049d44e8e4f320e9
Chamidu Lakshan <lakshanchamidu994@gmail.com>
2025-12-03 02:22:18 +0530

    Setup main.dart and add image background image to login page

---
+++ login_pages/lib/login_pade.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget{
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginScreen();
+}
+
+class _LoginScreen extends State <LoginPage>{
+
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(image: AssetImage("lib/assets/bg.png"),
+          fit: BoxFit.cover
+          )
+        ),
+      ),
+    );
+  }
+}
