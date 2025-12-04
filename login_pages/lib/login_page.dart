b12dd36d492b13577ca3eea7f42ba5c7f63fcb31
Chamidu Lakshan <lakshanchamidu994@gmail.com>
2025-12-04 13:21:10 +0530

    Add input text fields and container add

--- login_pages/lib/login_page.dart
+++ login_pages/lib/login_page.dart
@@ -17,7 +17,45 @@ class _LoginScreen extends State<LoginPage> {
             fit: BoxFit.cover,
           ),
         ),
-      ),
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            margin: EdgeInsets.all(50.0),
+            padding: EdgeInsets.all(20.0),
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.8),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            
+            child:Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(
+                    width: 250,
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Username',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  SizedBox(
+                    width: 250,
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Password",
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
     );
   }
 }
