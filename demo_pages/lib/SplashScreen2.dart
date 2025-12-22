e3c5af17436eb34f80b3608a705a864331e016e7
Chamidu Lakshan <lakshanchamidu994@gmail.com>
2025-12-22 11:02:28 +0530

    Auto commit - minor changes

--- demo_pages/lib/SplashScreen2.dart
+++ demo_pages/lib/SplashScreen2.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
 
-class SplashScreen2 extends StatefulWidget{
-  const SplashScreen2 ({super.key});
+class SplashScreen2 extends StatefulWidget {
+  const SplashScreen2({super.key});
 
   @override
-  State<SplashScreen2> createState()=> _SplashScreen2();
+  State<SplashScreen2> createState() => _SplashScreen2();
 }
 
-class _SplashScreen2 extends State<SplashScreen2>{
-  Widget build(BuildContext context){
+class _SplashScreen2 extends State<SplashScreen2> {
+  String selectedValue = "En";
+  List<String> languages = ['En', 'සිං', 'தமி'];
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green[900],
+      appBar: AppBar(
+        backgroundColor: Colors.green[900],
+        title: const Text(
+          "Uber Eats",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            fontSize: 32,
+            letterSpacing: 1.2,
+          ),
+        ),
+        actions: <Widget>[
+          DropdownButton<String>(value: selectedValue,),
+          IconButton(
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: const Text("Info button Pressed")),
+              );
+            },
+            icon: Icon(Icons.info_outline, color: Colors.white, size: 32),
+          ),
+        ],
+      ),
     );
   }
 }
