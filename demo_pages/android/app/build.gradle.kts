1c500dfadde2eba36147523aa4a00845016cd63f
Chamidu Lakshan <lakshanchamidu994@gmail.com>
2026-01-01 23:57:37 +0530

    Auto commit - minor changes

--- demo_pages/android/app/build.gradle.kts
+++ demo_pages/android/app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services") version "4.4.4" apply false
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -49,4 +49,3 @@ dependencies{
     implementation(platform("com.google.firebase:firebase-bom:32.2.0"))
     implementation("com.google.firebase:firebase-analytics-ktx")
 }
-apply(plugin = "com.google.gms.google-services")
