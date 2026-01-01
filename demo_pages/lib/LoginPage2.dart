8380836af4d120c518101401189602e3928c7cd2
Chamidu Lakshan <lakshanchamidu994@gmail.com>
2026-01-01 12:54:01 +0530

    Auto commit - minor changes

--- demo_pages/lib/LoginPage2.dart
+++ demo_pages/lib/LoginPage2.dart
@@ -40,12 +40,13 @@ class _LoginPage2 extends State<LoginPage2> {
         ],
       ),
 
-
       body: Column(
-        children: [Container(
-          padding: EdgeInsets.only(top: 40),
-          child: Lottie.asset("lib/assests/paymentCard.json"),
-        )],
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 40),
+            child: Lottie.asset('lib/assests/paymentCard.json'),
+          ),
+        ],
       ),
     );
   }
