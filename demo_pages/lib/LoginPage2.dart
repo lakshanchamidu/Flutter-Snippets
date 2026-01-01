13fac077fad6ea40ab3cd740dcee14cdc6dfd9f1
Chamidu Lakshan <lakshanchamidu994@gmail.com>
2026-01-01 12:23:51 +0530

    Auto commit - minor changes

--- demo_pages/lib/LoginPage2.dart
+++ demo_pages/lib/LoginPage2.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 
-class LoginPage2 extends StatefulWidget{
-  const LoginPage2 ({super.key});
+class LoginPage2 extends StatefulWidget {
+  const LoginPage2({super.key});
 
   @override
-  State<LoginPage2> createState()=> _LoginPage2();
+  State<LoginPage2> createState() => _LoginPage2();
 }
-class _LoginPage2 extends State<LoginPage2>{
 
-  Widget build(BuildContext context){
-    return Scaffold();
+class _LoginPage2 extends State<LoginPage2> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          "Wallet",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1.2,
+          ),
+        ),
+        actions: <Widget>[
+          Padding(padding: EdgeInsets.only(right: 20), child: Icon(Icons.lock_outline_rounded, size: 32, color: Colors.white,)),
+        ],
+      ),
+    );
   }
 }
