cab15ce6e49e1190aaa876a1fb01c1277b957453
Chamidu Lakshan <lakshanchamidu994@gmail.com>
2026-01-02 21:23:48 +0530

    Auto commit - minor changes

---
+++ demo_pages/lib/ECGScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ECGScreen extends StatefulWidget{
+  const ECGScreen ({super.key});
+
+  @override
+  State<ECGScreen> createState()=> _ECGScreen();
+}
+
+class _ECGScreen extends State <ECGScreen>{
+  Widget build(BuildContext context){
+    return Scaffold();
+  }
+}
