1ab591260dd7f31cc58c943c16c56907391d0cd6
Chamidu Lakshan <lakshanchamidu994@gmail.com>
2026-01-04 11:39:39 +0530

    Auto commit - minor changes

--- demo_pages/lib/ECGScreen.dart
+++ demo_pages/lib/ECGScreen.dart
@@ -1,14 +1,133 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class ECGScreen extends StatefulWidget{
-  const ECGScreen ({super.key});
+class ECGScreen extends StatefulWidget {
+  const ECGScreen({super.key});
 
   @override
-  State<ECGScreen> createState()=> _ECGScreen();
+  State<ECGScreen> createState() => _ECGScreen();
 }
 
-class _ECGScreen extends State <ECGScreen>{
-  Widget build(BuildContext context){
-    return Scaffold();
+class _ECGScreen extends State<ECGScreen> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(color: Colors.black.withOpacity(0.2)),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 20, top: 2),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        style: GoogleFonts.aBeeZee(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: "Hi Chamidu!",
+                            style: TextStyle(color: Colors.grey[800]),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(right: 20),
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(
+                          255,
+                          151,
+                          52,
+                          52,
+                        ).withOpacity(0.2),
+                        border: Border.all(
+                          color: Colors.white.withOpacity(0.3),
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.person_2_outlined,
+                              color: Colors.black,
+                              size: 28,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.notifications_outlined,
+                              size: 28,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Text(
+                  "Let's measure\nyour heart\nrate",
+                  style: GoogleFonts.aBeeZee(
+                    color: Colors.black,
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    height: 1.2,
+                  ),
+                ),
+              ),
+              Spacer(),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 90),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.red.withOpacity(0.2),
+                    border: Border.all(
+                      color: Colors.white70.withOpacity(0.3),
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.home_outlined, size: 35),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.insert_drive_file_outlined, size: 32),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.bar_chart_outlined, size: 35),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.heart_broken_outlined, size: 35),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
