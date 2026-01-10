0f582ed10b35b8ee101c6972c478cd45d66e894f
Chamidu Lakshan <lakshanchamidu994@gmail.com>
2026-01-11 00:00:38 +0530

    Auto commit - minor changes

--- demo_pages/lib/NavigationBars.dart
+++ demo_pages/lib/NavigationBars.dart
@@ -1,28 +1,37 @@
 import 'package:flutter/material.dart';
 import 'custom_bottom_nav_bar.dart';
 
-class NavigationBars extends StatefulWidget{
+class NavigationBars extends StatefulWidget {
   const NavigationBars({super.key});
 
   @override
-  State<NavigationBars> createState()=> _NavigationBars();
+  State<NavigationBars> createState() => _NavigationBars();
 }
 
-class _NavigationBars extends State<NavigationBars>{
-  Widget build(BuildContext context){
+class _NavigationBars extends State<NavigationBars> {
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green[900],
       body: Stack(
         children: [
           Center(
-            child: Text("Custom Navbar", style: TextStyle(
-              fontSize: 22,
-              color: Colors.white
-            ),),
+            child: Text(
+              "Custom Navbar",
+              style: TextStyle(fontSize: 22, color: Colors.white),
+            ),
           ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: CustomBottomNavBar(),
+          Positioned(
+            bottom: 50,
+            left: MediaQuery.of(context).size.width / 2 - 30,
+            child: Container(
+              height: 56,
+              width: 56,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+              ),
+              child: Icon(Icons.add, color: Colors.black, size: 28),
+            ),
           ),
         ],
       ),
