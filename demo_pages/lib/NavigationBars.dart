c8a7d836a1d1946508f3a40aa9944edaaaff7bc4
Chamidu Lakshan <lakshanchamidu994@gmail.com>
2026-01-10 22:33:36 +0530

    Auto commit - minor changes

--- demo_pages/lib/NavigationBars.dart
+++ demo_pages/lib/NavigationBars.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class NavigationBars extends StatefulWidget{
   const NavigationBars({super.key});
@@ -11,6 +12,25 @@ class _NavigationBars extends State<NavigationBars>{
   Widget build(BuildContext context){
     return Scaffold(
       backgroundColor: Colors.green[900],
+      body: Stack(
+        children: [
+          Center(
+            child: Text("Custom Navbar", style: TextStyle(
+              fontSize: 22,
+              color: Colors.white
+            ),),
+          )
+          Positioned(
+            bottom: 35,
+            left: MediaQuery.of(context).size.width / 2 - 30,
+            child: FloatingActionButton(
+              backgroundColor: Colors.white,
+              onPressed: () {},
+              child: Icon(Icons.add, color: Colors.black),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ demo_pages/lib/bottom_bar_clipper.dart
@@ -0,0 +1,39 @@
+class BottomBarClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+
+    path.lineTo(0, 0);
+    path.lineTo(size.width * 0.35, 0);
+
+    path.quadraticBezierTo(
+      size.width * 0.40,
+      0,
+      size.width * 0.42,
+      20,
+    );
+
+    path.arcToPoint(
+      Offset(size.width * 0.58, 20),
+      radius: Radius.circular(30),
+      clockwise: false,
+    );
+
+    path.quadraticBezierTo(
+      size.width * 0.60,
+      0,
+      size.width * 0.65,
+      0,
+    );
+
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
